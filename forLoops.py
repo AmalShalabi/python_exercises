53d3dcf16309050031502cc0606dae402e9c626f
unknown <mouni.shalaby@gmail.com>
2023-06-19 23:06:44 +0300

    guess number game

---
+++ forLoops.py
@@ -0,0 +1,14 @@
+prices=[20,30,60]
+total=0
+for price in prices:
+    total+=price
+print(f"sum: {total}")
+
+# for item in ['Aamal', 'Jumana', 'Maya']:
+#     print(item)
+
+# for item in range(100):
+#      print(item)
+
+# for item in range(5,10,2):
+#     print(item)
---
+++ maxNumList.py
@@ -0,0 +1,6 @@
+numbers=[1,50,63,8,6,9]
+max_number=numbers[0]
+for number in numbers:
+    if number > max_number:
+        max_number=number
+print(max_number)
