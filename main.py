190b6d46cb3e824f39d2da74d6a51fb57848d0c1
unknown <mouni.shalaby@gmail.com>
2023-06-19 15:51:57 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+command=""
+while True:
+    command=input('> ').lower()
+    if command=='start':
+       print('Car started ready to go')
+    elif command=="stop":
+         print("Car Stoped")
+    elif command=="help":
+        print("""
+start- to start the car
+stop-to stop the car
+quit- to exit
+        
+        """)
+    elif command=="quit":
+        break
+    else:
+            print("I don't understand that")
+
+
